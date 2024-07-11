cd3c2b283f6cf6141d80c8ddb8df7b1e0ac44423
fernando <tommy0101@hotmail.com.tw>
2024-07-11 14:10:37 +0300

    workflow added

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 from sklearn.datasets import make_regression
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.metrics import confusion_matrix
 import pickle
 import os
@@ -8,7 +8,7 @@ import os
 X, y = make_regression(10000,n_features = 10)
 
 # Train a model
-reg = Lasso().fit(X, y.ravel())
+reg = Ridge().fit(X, y.ravel())
 # Print out training r2
 print(reg.score(X,y.ravel() ))
 
